bb44bf6a364050d7cdd2aef671acf3349bb62961
James Custer <jamescuster0121@gmail.com>
2019-11-11 09:40:14 -0600

    clean addEditModules documentation and modalInputs function

--- R/addEditModules.R
+++ R/addEditModules.R
@@ -1,12 +1,12 @@
 #' Add database entry button: UI function
 #'
-#' This function and \code{\link{addModule}} are used in conjunction to add the UI and
-#' server elements necessary to add a row to a given table in a database
+#' This function and \code{\link{addModule}} are used in conjunction to add the
+#' UI and server elements necessary to add a row to a given table in a database
 #'
 #' @param id character name for the namespace of the module
 #'
-#' @return A shiny \code{\link[shiny]{actionButton}} which opens a modal to allow user to
-#'   input data to be added to a database
+#' @return A shiny \code{\link[shiny]{actionButton}} which opens a modal to
+#'   allow user to input data to be added to a database
 #'
 #' @seealso \code{\link{addModule}}
 #'
@@ -38,20 +38,21 @@ addModuleUI <- function(id) {
 #' This function and \link{addModuleUI} are used in conjection to add the UI and
 #' server elements necessary to add a row to a given table in a database
 #'
-#' @param input,output,session These parameters are handled by \code{\link[shiny]{callModule}}
-#'   and can be ignored.
+#' @param input,output,session These parameters are handled by
+#'   \code{\link[shiny]{callModule}} and can be ignored.
 #' @param modalTitle Character string for title to be displayed at the top of
 #'   the modal.
 #' @param inputData a \code{data.frame} containing columns \code{ids, labels,
-#'   type} which correspond to the field names of the table in the database, lables to display for the input
-#'   in the shiny UI, and the type of shiny input to be used.
-#' @param db a \code{\link[DBI:DBIConnection-class]{DBIConnection}} object, as returned by
-#'   \code{\link[DBI]{dbConnect}}. In other words, the object the database connection
-#'   is saved to.
+#'   type} which correspond to the field names of the table in the database,
+#'   lables for the input in the shiny UI, and the type of shiny input to be
+#'   used.
+#' @param db a \code{\link[DBI:DBIConnection-class]{DBIConnection}} object, as
+#'   returned by \code{\link[DBI]{dbConnect}}. In other words, the object the
+#'   database connection is saved to.
 #' @param dbTable The database table the new data will be added to.
 #'
-#' @return Shiny \code{\link[shiny]{observeEvent}}'s which control actions when the
-#' add button is pressed, as well as the save button in the modal.
+#' @return Shiny \code{\link[shiny]{observeEvent}}'s which control actions when
+#'   the add button is pressed, as well as the save button in the modal.
 #'
 #' @seealso \code{\link{addModuleUI}}
 #'
@@ -86,57 +87,72 @@ addModule <- function(input, output, session, modalTitle, inputData, db, dbTable
 
 
 
-#' Hello
+#' Create list of shiny inputs for modal
+#'
+#' Takes a \code{data.frame} containing information about shiny inputs and
+#' additional optional parameters to create list of shiny inputs to be displayed
+#' in a modal
+#'
+#' @param session The \code{session} object passed to function given to
+#'   shinyServer. This should usually be \code{session = session}.
+#' @inheritParams addModule
+#' @param values Optional argument to be used when the inputs are being
+#'   populated from an observation in the database. (NEED MORE DOCUMENTATION
+#'   HERE ONCE THE EDIT FUNCTIONALITY IS BUILT OUT)
+#' @param choices Optional argument to provide the choices for
+#'   \code{\link[shiny]{selectInput}} and \code{\link[shiny]{selectizeInput}}
+#'   inputs. NEED TO VERIFY THAT THIS WORKS. FOR THE EXAMPLE i USED TO DEVELOP
+#'   THIS i DID NOT HAVE A SELECT INPUT
 #'
 #' @export
-modalInputs <- function(ids, labels, type, values, df, choices, session) {
-  fields <- list()
-  for (i in seq_along(ids)) {
-    if (type[i] == "skip") {
-      fields[[ids[i]]] <- NULL
-    }
-    else if (type[i] == "textInput") {
-      value <- ifelse(missing(values) || is.na(values[ids[i]]), "", values[ids[i]])
-      fields[[ids[i]]] <- textInput(inputId = session$ns(ids[i]),
-                                    label = labels[i],
-                                    value = value,
-                                    width = 400)
-    }
-    else if (type[i] == "selectizeInput") {
-      value <- ifelse(missing(values) || is.na(values[ids[i]]), "", values[ids[i]])
-      fields[[ids[i]]] <- selectizeInput(inputId = session$ns(ids[i]),
-                                         label = labels[i],
-                                         choices = c("", choices[[ids[[i]]]]),
-                                         selected = value,
-                                         width = 400)
-    }
-    else if (type[i] == "selectInput") {
-      value <- ifelse(missing(values) || is.na(values[ids[i]]), "", values[ids[i]])
-      fields[[ids[i]]] <- selectInput(inputId = session$ns(ids[i]),
-                                      label = labels[i],
-                                      choices = c("", choices[[ids[[i]]]]),
-                                      width = 400)
-    }
-    else if (type[i] == "textAreaInput") {
-      value <- ifelse(missing(values) || is.na(values[ids[i]]), "", values[ids[i]])
-      fields[[ids[i]]] <- textAreaInput(inputId = session$ns(ids[i]),
-                                        label = labels[i],
-                                        value = value,
-                                        width = "400px",
-                                        height = "102px")
-    }
-    else if (type[i] == "dateInput") {
-      value <- ifelse(missing(values) || is.na(values[ids[i]]), "", values[ids[i]])
-      fields[[ids[i]]] <- dateInput(inputId = session$ns(ids[i]),
-                                    label = labels[i],
-                                    value = value,
-                                    width = 400)
-    }
-    else if (type[i] == "actionButton") {
-      fields[[ids[i]]] <- actionButton(inputId = session$ns(ids[i]),
-                                       label = labels[i],
-                                       style = "margin-left: 20px; margin-top: 24px; height: 34px;")
-    }
-  }
+modalInputs <- function(session, inputData, values, choices) {
+  fields <-
+    apply(
+      inputData, 1,
+      function(x, values) {
+        value <- ifelse(missing(values) || is.na(values[x["ids"]]), "", values[x["ids"]])
+        if (x["type"] == "skip") {
+          NULL
+        }
+        else if (x["type"] == "textInput") {
+          textInput(inputId  = session$ns(x["ids"]),
+                    label = x["labels"],
+                    value = value,
+                    width = 400)
+        }
+        else if (x["type"] == "selectizeInput") {
+          selectizeInput(inputId  = session$ns(x["ids"]),
+                         label = x["labels"],
+                         choices = c("", choices[[ids[[i]]]]),
+                         selected = value,
+                         width = 400)
+        }
+        else if (x["type"] == "selectInput") {
+          selectInput(inputId  = session$ns(x["ids"]),
+                      label = x["labels"],
+                      choices = c("", choices[[ids[[i]]]]),
+                      width = 400)
+        }
+        else if (x["type"] == "textAreaInput") {
+          textAreaInput(inputId  = session$ns(x["ids"]),
+                        label = x["labels"],
+                        value = value,
+                        width = "400px",
+                        height = "102px")
+        }
+        else if (x["type"] == "dateInput") {
+          dateInput(inputId  = session$ns(x["ids"]),
+                    label = x["labels"],
+                    value = value,
+                    width = 400)
+        }
+        else if (x["type"] == "actionButton") {
+          actionButton(inputId  = session$ns(x["ids"]),
+                       label = x["labels"],
+                       style = "margin-left: 20px; margin-top: 24px; height: 34px;")
+        }
+      }
+    )
   fields
 }
+
