a39f995df99f6a5a08168f1e4ec75ddea5f7e890
James Custer <jamescuster0121@gmail.com>
2020-03-02 09:54:08 -0600

    update internal app global.r

--- inst/shinyExamples/irisApp/app/global.r
+++ inst/shinyExamples/irisApp/app/global.r
@@ -58,182 +58,80 @@ irisStaticChoices <- list(
 )
 
 
-# irisFilters <- data.frame(
-#   ids = c("speciesFilter", "smellFilter"),
-#   labels = c("Species", "Smell"),
-#   type = c("selectizeInput", "selectInput"),
-#   choicesTable = c("flowers", "static"),
-#   choicesValues = c("flowerID", NA),
-#   choicesLabels = c("flowerName", NA),
-#   stringsAsFactors = FALSE
-# )
+irisFilters <- data.frame(
+  ids = c("speciesFilter", "smellFilter"),
+  labels = c("Species", "Smell"),
+  filterColumnIds = c("Species", "smell"),
+  type = c("selectizeInput", "selectInput"),
+  choicesTable = c("flowers", "static"),
+  choicesValues = c("flowerID", NA),
+  choicesLabels = c("flowerName", NA),
+  stringsAsFactors = FALSE
+)
 
 
 # Flowers -----------------------------------------------------------------
 flowerInputs <- data.frame(
-  ids = c("flowerID", "flowerName"),
-  labels = c("flowerID", "Flower Name"),
-  type = c("skip", "textInput"),
+  ids = c("flowerID", "flowerName", "flowerName2"),
+  labels = c("flowerID", "Flower Name", "Flower Name 2"),
+  type = c("skip", "textInput", "textInput"),
   stringsAsFactors = FALSE
 )
 
 
 
-
-
-dtModuleUI <- function(id, filterData = NULL, staticChoices = NULL) {
-  ns <- shiny::NS(id)
-
-  # Create filter inputs if they exists
-  if (!is.null(filterData)) {
-    filters <-
-      apply(
-        filterData, 1,
-        function(x) {
-          if (x["ids"] == filterData$ids[1]) {
-            style <- NULL
-          } else {
-            style <- "margin-left: 20px;"
-          }
-          div(
-            selectizeInput(
-              inputId = ns(x["ids"]),
-              label = x["labels"],
-              choices = c("All", staticChoices[[x["ids"]]])
-            ),
-            style = style
-          )
+modalInputs2 <- function(ns, inputData, values = NULL, choices) {
+  inputData <- inputData[inputData$type != "skip", ]
+  fields <-
+    apply(
+      inputData, 1,
+      function(x, values) {
+        value <- ifelse(is.null(values) || is.na(values[x["ids"]]),
+                        "", values[x["ids"]])
+        if (x["type"] == "skip") {
+          NULL
         }
-      )
-  } else {
-    filters <- NULL
-  }
-
-  list(
-    div(
-      filters,
-      style = "display: flex; align-items: flex-start;"
-    ),
-    DT::dataTableOutput(ns("dt"))
-  )
+        else if (x["type"] == "textInput") {
+          shiny::textInput(inputId  = ns(x["ids"]),
+                           label = x["labels"],
+                           value = value,
+                           width = 400)
+        }
+        else if (x["type"] == "selectizeInput") {
+          shiny::selectizeInput(inputId  = ns(x["ids"]),
+                                label = x["labels"],
+                                choices = c("", choices[[x["ids"]]]),
+                                selected = value,
+                                width = 400)
+        }
+        else if (x["type"] == "selectInput") {
+          shiny::selectInput(inputId  = ns(x["ids"]),
+                             label = x["labels"],
+                             choices = c("", choices[[x["ids"]]]),
+                             width = 400)
+        }
+        else if (x["type"] == "textAreaInput") {
+          shiny::textAreaInput(inputId  = ns(x["ids"]),
+                               label = x["labels"],
+                               value = value,
+                               width = "400px",
+                               height = "102px")
+        }
+        else if (x["type"] == "dateInput") {
+          value <- if (value == "") as.Date(NA) else value
+          shiny::dateInput(inputId  = ns(x["ids"]),
+                           label = x["labels"],
+                           value = value,
+                           width = 400)
+        }
+        else if (x["type"] == "actionButton") {
+          shiny::actionButton(inputId  = ns(x["ids"]),
+                              label = x["labels"],
+                              style = "margin-left: 20px;
+                                margin-top: 24px;
+                                height: 34px;")
+        }
+      }, values = values
+    )
+  fields
 }
-
-
-# dtModule <- function(input, output, session, reactiveData, dbTable, filterData = NULL, staticChoices) {
-#   # used to presreve selected row on reloads if row is selected
-#   selected <- NULL
-#   shiny::observeEvent(input$dt_rows_selected, {
-#     selected <<- input$dt_rows_selected
-#   })
-#
-#
-#
-#   filterReactive <- reactive({
-#     # If there is no filter data, return that reactiveData dbTable as is
-#     if (is.null(filterData)) {
-#       return(reactiveData[[dbTable]])
-#     }
-#
-#     # If there is filter data, apply the filters
-#     filtered <- reactiveData[[dbTable]] %>%
-#       {
-#         if (input[[filterData$ids[1]]] != "All") {
-#           filter(., Species == input[[filterData$ids[1]]])
-#         } else {
-#           .
-#         }
-#       } %>%
-#       {
-#         if (input[[filterData$ids[2]]] != "All") {
-#           filter(., smell == input[[filterData$ids[2]]])
-#         } else {
-#           .
-#         }
-#       }
-#     return(filtered)
-#   })
-#
-#   # Creates the datatable
-#   output$dt <-
-#     DT::renderDataTable(
-#       DT::datatable(
-#         filterReactive(),
-#         selection = list(
-#           mode = "single",
-#           selected = selected
-#         ),
-#         rownames = FALSE,
-#         options = list(
-#           dom = '<"top"fl> t <"bottom"ip>',
-#           order = list(0, "desc")
-#         )
-#       ),
-#       server = TRUE
-#     )
-#
-#   if (!is.null(filterData)) {
-#     dtFilterUpdates(input, output, session, filterData = filterData, reactiveData = reactiveData)
-#   }
-# }
-#
-#
-# addModule <- function(input, output, session,
-#                       modalTitle, inputData, db, dbTable, reactiveData,
-#                       staticChoices = NULL) {
-#   # controls what happens when add is pressed
-#   shiny::observeEvent(input$add, {
-#     # Checks inputData for select input types, if present, gathers the choices
-#     if (any(grepl("select", inputData$type))) {
-#       choices <- choicesReactive(inputData, reactiveData, staticChoices)
-#     }
-#
-#     # Creates modal
-#     shiny::showModal(
-#       shiny::modalDialog(
-#         title = modalTitle,
-#         modalInputs(
-#           session = session,
-#           inputData = inputData,
-#           choices = choices
-#         ),
-#         footer =
-#           list(
-#             shiny::modalButton("Cancel"),
-#             shiny::actionButton(session$ns("insert"), "Save")
-#           )
-#       )
-#     )
-#   })
-#
-#   # Controls what happens when Save is pressed
-#   shiny::observeEvent(input$insert, {
-#     insertCallback(input, output, session, inputData$ids, db, dbTable)
-#     shiny::removeModal()
-#   })
-# }
-#
-# choicesReactive <- function(inputData, reactiveData, staticChoices) {
-#   choicesReact <- shiny::reactive({
-#     choices <-
-#       lapply(
-#         inputData$ids,
-#         function(x) {
-#           if (grepl("select", inputData[inputData$ids == x, "type"])) {
-#             if (tolower(inputData[inputData$ids == x, "choicesTable"]) == "static") {
-#               staticChoices[[x]]
-#             } else {
-#               valueLabel(
-#                 df = reactiveData[[inputData[inputData$ids == x, "choicesTable"]]],
-#                 value = inputData[inputData$ids == x, "choicesValues"],
-#                 label = inputData[inputData$ids == x, "choicesLabels"])
-#             }
-#           } else {
-#             return(NA)
-#           }
-#         }
-#       )
-#     choices <- stats::setNames(choices, inputData$ids)
-#     return(choices)
-#   })
-#   return(choicesReact())
-# }
